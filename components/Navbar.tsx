f8afbab85ab05aec38b34e97ba0630ab7d4e830c
Jonibek <munirovjonibek@gmail.com>
2024-07-22 15:48:26 +0500

    nav

--- components/Navbar.tsx
+++ components/Navbar.tsx
@@ -1,7 +1,22 @@
+import Link from "next/link";
 import React from "react";
 
 const Navbar = () => {
-  return <div>Navbar</div>;
+  return (
+    <div className="navbar">
+      <div className="navLeft">
+        <Link className="navLink" href="/about">
+          О нас
+        </Link>
+        <Link className="navLink" href="/contact">
+          Контакты
+        </Link>
+        <Link className="navLink" href="/contact">
+          Контакты
+        </Link>
+      </div>
+    </div>
+  );
 };
 
 export default Navbar;
