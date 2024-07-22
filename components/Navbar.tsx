4c529cd0bae55f6b79f35d1a574bf4837cb0f96e
Jonibek <munirovjonibek@gmail.com>
2024-07-22 16:22:27 +0500

    navbar and header

--- components/Navbar.tsx
+++ components/Navbar.tsx
@@ -1,19 +1,41 @@
+import Image from "next/image";
 import Link from "next/link";
 import React from "react";
-
+import styles from "./navbar.module.css";
 const Navbar = () => {
   return (
-    <div className="navbar">
-      <div className="navLeft">
-        <Link className="navLink" href="/about">
-          О нас
-        </Link>
-        <Link className="navLink" href="/contact">
-          Контакты
-        </Link>
-        <Link className="navLink" href="/contact">
-          Контакты
-        </Link>
+    <div className={styles.navbar}>
+      <div className={styles.navContent}>
+        <div className={styles.navLinks}>
+          <Link className={styles.navLink} href="/about">
+            О нас
+          </Link>
+          <Link className={styles.navLink} href="/contact">
+            Контакты
+          </Link>
+          <Link className={styles.navLink} href="/baguette">
+            Оформление в багет
+          </Link>
+          <Link className={styles.navLink} href="/services">
+            Услуги
+          </Link>
+        </div>
+        <div className={styles.searchBox}>
+          <input
+            className={styles.searchInput}
+            type="text"
+            placeholder="Поиск по каталогу"
+          />
+          <button className={styles.searchButton}>
+            <Image
+              className={styles.searchIcon}
+              src="/search-icon.svg"
+              alt="search-icon"
+              width={19}
+              height={19}
+            />
+          </button>
+        </div>
       </div>
     </div>
   );
