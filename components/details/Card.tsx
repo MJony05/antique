1e7ce5ffa1bf8556b90d8506530972276f44c034
Jonibek <munirovjonibek@gmail.com>
2024-07-25 10:22:53 +0500

    cards style

--- components/details/Card.tsx
+++ components/details/Card.tsx
@@ -1,7 +1,46 @@
 import React from "react";
-
-const Card = () => {
-  return <div>Card</div>;
+import styles from "./card.module.css";
+import Image from "next/image";
+interface CardProps {
+  id: string;
+  image: string;
+  title: string;
+  category: string;
+  price: number;
+}
+const Card = ({ data }: { data: CardProps }) => {
+  return (
+    <div className={styles.card}>
+      <Image
+        className={styles.cardImage}
+        src={data.image}
+        alt="cardImage"
+        width={250}
+        height={280}
+      />
+      <div className={styles.cardContent}>
+        <div className={styles.cardInfo}>
+          <p className={styles.cardCategory}>{data.category}</p>
+          <h3 className={styles.cardTitle}>{data.title}</h3>
+        </div>
+        <div className={styles.cardAction}>
+          <div className={styles.cardLine}>
+            <p className={styles.cardPrice}>{data.price} руб</p>
+            <button className={styles.linkButton}>
+              Смотреть
+              <Image
+                src={"/grey-arrow.svg"}
+                alt="arrow"
+                width={23}
+                height={20}
+              />
+            </button>
+          </div>
+          <button className={styles.addButton}>В корзину</button>
+        </div>
+      </div>
+    </div>
+  );
 };
 
 export default Card;
--- components/home/NewItems.tsx
+++ components/home/NewItems.tsx
@@ -1,11 +1,44 @@
 import React from "react";
 import Header from "../Header";
 import Title from "../details/Title";
-
+import styles from "./newitems.module.css";
+import Card from "../details/Card";
+const arr = [
+  {
+    id: "1",
+    image: "/card-example2.png",
+    title: "Павел I. Прижизненный портрет. 1780",
+    category: "Флора и Фауна",
+    price: 30000,
+  },
+  {
+    id: "2",
+    image: "/card-example2.png",
+    title:
+      "Московский Императорский Воспитательный дом. 1857г. Антикварная гравюра",
+    category: "Флора и Фауна",
+    price: 30000,
+  },
+  {
+    id: "3",
+    image: "/card-example3.png",
+    title: "Московские зарисовки. 1890г. Хакер/Бренд’амур. Старинная гравира",
+    category: "Флора и Фауна",
+    price: 30000,
+  },
+];
 const NewItems = () => {
   return (
-    <div>
+    <div className={styles.newItems}>
       <Title text="Новинки" size={36} />
+      <div className={styles.newItemsContent}>
+        {arr.map((item) => (
+          <Card data={item} key={item.id} />
+        ))}
+        {arr.map((item) => (
+          <Card data={item} key={item.id} />
+        ))}
+      </div>
     </div>
   );
 };
