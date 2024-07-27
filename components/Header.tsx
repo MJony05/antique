159e450cb7263fb60d69c4b1492c0fa44d1bd277
Jonibek <munirovjonibek@gmail.com>
2024-07-27 19:41:10 +0500

    homepage responsive

--- components/Header.tsx
+++ components/Header.tsx
@@ -6,6 +6,22 @@ const Header = () => {
   return (
     <nav className={styles.navbar}>
       <div className={styles.navLeft}>
+        <div className={styles.searchBox}>
+          <input
+            className={styles.searchInput}
+            type="text"
+            placeholder="Поиск по каталогу"
+          />
+          <button className={styles.searchButton}>
+            <Image
+              className={styles.searchIcon}
+              src="/search-icon.svg"
+              alt="search-icon"
+              width={19}
+              height={19}
+            />
+          </button>
+        </div>
         <div className={styles.navLeftItem}>
           <div className={styles.navLeftItemImage}>
             <Image
@@ -37,8 +53,20 @@ const Header = () => {
         </div>
       </div>
       <div className={styles.navRight}>
-        <Button text="Заказать звонок" />
-        <Image src="/cart-icon.svg" alt="cart" width={44} height={44} />
+        <div className={styles.navRightItem}>
+          <Button text="Заказать звонок" />
+        </div>
+
+        <Image
+          className={styles.cart}
+          src="/cart-icon.svg"
+          alt="cart"
+          width={44}
+          height={44}
+        />
+        <div className={styles.burger}>
+          <Image src="/burger.png" alt="burger" width={48} height={48} />
+        </div>
       </div>
     </nav>
   );
--- components/home/AboutComponent.tsx
+++ components/home/AboutComponent.tsx
@@ -1,18 +1,38 @@
-import React from "react";
+"use client";
+import React, { useState } from "react";
 import styles from "./aboutComponent.module.css";
 import Image from "next/image";
 import Button from "../details/button";
 import Title from "../details/Title";
+
 const AboutComponent = () => {
+  const [currentSlide, setCurrentSlide] = useState(0);
+
+  const images = [
+    { src: "/about-1.png", alt: "about1", width: 283, height: 368 },
+    { src: "/about-2.png", alt: "about2", width: 174, height: 357 },
+    { src: "/about-3.png", alt: "about3", width: 174, height: 309 },
+  ];
+
+  const nextSlide = () => {
+    setCurrentSlide((prevSlide) => (prevSlide + 1) % images.length);
+  };
+
+  const prevSlide = () => {
+    setCurrentSlide((prevSlide) =>
+      prevSlide === 0 ? images.length - 1 : prevSlide - 1
+    );
+  };
+
   return (
-    <div className={styles.aboutHome}>
+    <>
       <div className={styles.top}>
         <Title text="О нас" size={32} />
         <div className={styles.pagination}>
-          <button className={styles.button}>
+          <button onClick={prevSlide} className={styles.button}>
             <Image src={"/arrow.svg"} alt="arrow" width={30} height={18} />
           </button>
-          <button className={styles.button}>
+          <button onClick={nextSlide} className={styles.button}>
             <Image
               src={"/arrow.svg"}
               style={{ transform: "rotate(180deg)" }}
@@ -38,31 +58,42 @@ const AboutComponent = () => {
           </p>
           <Button text={"Подробнее о нас"} />
         </div>
+        <div className={styles.pagination2}>
+          <button onClick={prevSlide} className={styles.button}>
+            <Image src={"/arrow.svg"} alt="arrow" width={30} height={18} />
+          </button>
+          <button onClick={nextSlide} className={styles.button}>
+            <Image
+              src={"/arrow.svg"}
+              style={{ transform: "rotate(180deg)" }}
+              alt="arrow"
+              width={30}
+              height={18}
+            />
+          </button>
+        </div>
         <div className={styles.contentRight}>
-          <Image
-            className={styles.image1}
-            src={"/about-1.png"}
-            alt={"about1"}
-            width={283}
-            height={368}
-          />
-          <Image
-            className={styles.image2}
-            src={"/about-2.png"}
-            alt={"about2"}
-            width={174}
-            height={357}
-          />
-          <Image
-            className={styles.image3}
-            src={"/about-3.png"}
-            alt={"about3"}
-            width={174}
-            height={309}
-          />
+          <div className={styles.slider}>
+            {images.map((image, index) => (
+              <div
+                key={index}
+                className={`${styles.slide} ${
+                  index === currentSlide ? styles.active : ""
+                }`}
+              >
+                <Image
+                  className={styles.image}
+                  src={image.src}
+                  alt={image.alt}
+                  width={image.width}
+                  height={image.height}
+                />
+              </div>
+            ))}
+          </div>
         </div>
       </div>
-    </div>
+    </>
   );
 };
 
